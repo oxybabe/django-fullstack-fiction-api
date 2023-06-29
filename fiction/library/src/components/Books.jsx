4d719e22c30e9e4a05f11b5801971e12d0b68daf
Elia Partin <arenaselia@gmail.com>
2023-06-29 16:04:25 -0400

    fixed virtual env

--- fiction/library/src/components/Books.jsx
+++ fiction/library/src/components/Books.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ text, onClick }) => {
   return (
     <>
-      <h1>Delete Book</h1>
+      <h1>Book</h1>
       <button onClick={onClick}>{text}</button>
     </>
   );
---
+++ fiction/library/src/components/DeleteBook.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+  // export default function deleteBook() {
+  //   const fetch('http://127.0.0.1:8000/books/1/', method: "DELETE")
+  //   .then(response => await response.json())
+  //   .then(response => {
+  //     console.log(response.data);
+  //   })
+  //   .catch(error => { console.error(error )});
+  // };
